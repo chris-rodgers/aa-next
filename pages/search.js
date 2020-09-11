a79de14d476f675d32b95e92c1a4c04314036768
chris.rodgers <crodgers@alternativeairlines.com>
2020-09-11 11:35:57 +0100

    style tweaks

--- pages/search.js
+++ pages/search.js
@@ -52,19 +52,19 @@ export default function Search() {
                 <path d="M4.25,5.61C6.27,8.2,10,13,10,13v6c0,0.55,0.45,1,1,1h2c0.55,0,1-0.45,1-1v-6c0,0,3.72-4.8,5.74-7.39 C20.25,4.95,19.78,4,18.95,4H5.04C4.21,4,3.74,4.95,4.25,5.61z" fill="#384756" />
             </svg>
             <div className={styles.header__row}>
-                <button className={styles.header__item} onClick={handleSelectModal}>Flexible Changes</button>
-                <button className={styles.header__item} onClick={handleSelectModal}>Flexible Cancellation</button>
-                <button className={styles.header__item} onClick={handleSelectModal}>Stops</button>
-                <button className={styles.header__item} onClick={handleSelectModal}>Bags</button>
-                <button className={styles.header__item} onClick={handleSelectModal}>Airlines</button>
-                <button className={styles.header__item} onClick={handleSelectModal}>Class</button>
-                <button className={styles.header__item} onClick={handleSelectModal}>Times</button>
-                <button className={styles.header__item} onClick={handleSelectModal}>Duration</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Flexible Changes</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Flexible Cancellation</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Stops</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Bags</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Airlines</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Class</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Times</button>
+                <button className={styles.header__item} onClick={handleSelectModal}><small>▼</small> Duration</button>
             </div>
         </div>
         <AutoSizer>
             {({ height, width }) => (
-                <List height={height - 76} width={width} itemCount={results.priceGroups.length} itemSize={(44 * segmentCount) + 10}>
+                <List height={height - 88} width={width} itemCount={results.priceGroups.length} itemSize={(44 * segmentCount) + 10}>
                     {({ index, style }) => {
                         const priceGroup = results.priceGroups[index];
                         return <div style={style}>

6a38288b5d8f0906c95a34dcf5960b2ed99e7d24
chris.rodgers <crodgers@alternativeairlines.com>
2020-09-11 09:14:48 +0100

    experement with react-window

--- pages/search.js
+++ pages/search.js
@@ -4,7 +4,8 @@ import styles from "../styles/search/search.module.scss";
 import flight from "../styles/search/flight.module.scss";
 import withModal from '../components/Modal';
 import PassengerPicker from "../modules/PassengerPicker";
-
+import { VariableSizeList as List } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 var filters = {
     PassengerPicker
@@ -15,7 +16,6 @@ export default function Search() {
     const [selectedPriceGroup, setSelectedPriceGroup] = React.useState();
     const [selectedModal, setSelectedModal] = React.useState();
     const Modal = withModal(filters[selectedModal]);
-    console.log(selectedModal);
 
     const handleSelectPriceGroup = e => {
         setSelectedPriceGroup(Number(e.currentTarget.dataset.id))
@@ -52,52 +52,59 @@ export default function Search() {
                 <button className={styles.header__item} onClick={handleSelectModal}>Duration</button>
             </div>
         </div>
-        <div>
-            {results.priceGroups ? results.priceGroups.map(priceGroup => {
-                return <div className={styles['price-group']} key={priceGroup.id} onClick={handleSelectPriceGroup} data-id={priceGroup.id}>
-                    <div className={styles['price-group__segments']}>
-                        {priceGroup.groupSegments.map(groupSegments => {
-                            const segment = groupSegments.segments[0]; // May have to loop through if we do grouping again
-                            const stops = segment.legs.length - 1;
-                            // console.log(groupSegments.segments)
-                            return <div className={styles.segment} key={segment.itineraryGroupId}>
-                                <div className={styles.segment__logo}>
-                                    <img src={`/airlinelogos/${segment.operatingAirlineCode.toLowerCase()}_logo.gif`} />
+        <AutoSizer>
+            {({ height, width }) => (
+                <List height={height} width={width} itemCount={results.priceGroups.length} itemSize={getItemSize}>
+                    {({ index, style }) => {
+                        const priceGroup = results.priceGroups[index];
+                        return <div style={style}>
+                            <div className={styles['price-group']} key={priceGroup.id} onClick={handleSelectPriceGroup} data-id={priceGroup.id} data-index={index}>
+                                <div className={styles['price-group__segments']}>
+                                    {priceGroup.groupSegments.map(groupSegments => {
+                                        const segment = groupSegments.segments[0]; // May have to loop through if we do grouping again
+                                        const stops = segment.legs.length - 1;
+                                        // console.log(groupSegments.segments)
+                                        return <div className={styles.segment} key={segment.itineraryGroupId}>
+                                            <div className={styles.segment__logo}>
+                                                <img src={`/airlinelogos/${segment.operatingAirlineCode.toLowerCase()}_logo.gif`} />
+                                            </div>
+                                            <div className={styles.segment__content}>
+                                                <div className={styles.segment__airports}>
+                                                    <div className={styles.airport}>
+                                                        <div className={styles.airport__time}>{segment.departureTime}</div>
+                                                        <div className={styles.airport__code}>{segment.departureAirportCode}</div>
+                                                    </div>
+                                                    <div className={styles.segment__legs}>
+                                                        <small>{segment.eft}</small>
+                                                        <div>{stops ? `${stops} Stop(s)` : 'Direct'}</div>
+                                                    </div>
+                                                    <div className={styles.airport}>
+                                                        <div className={styles.airport__time}>{segment.arrivalTime}</div>
+                                                        <div className={styles.airport__code}>{segment.arrivalAirportCode}</div>
+                                                    </div>
+                                                </div>
+                                                <div className={styles.segment__tags}>
+                                                    {segment.seatsAvailable ? <div className={styles.tag}>
+                                                        {segment.seatsAvailable} seats left
+                                                    </div> : null}
+                                                    {segment.flexibleChange ? <div className={styles.tag}>
+                                                        Flexible Change
+                                                    </div> : null}
+                                                    {segment.flexibleCancellation ? <div className={styles.tag}>
+                                                        Flexible Cancellation
+                                                    </div> : null}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    })}
                                 </div>
-                                <div className={styles.segment__content}>
-                                    <div className={styles.segment__airports}>
-                                        <div className={styles.airport}>
-                                            <div className={styles.airport__time}>{segment.departureTime}</div>
-                                            <div className={styles.airport__code}>{segment.departureAirportCode}</div>
-                                        </div>
-                                        <div className={styles.segment__legs}>
-                                            <small>{segment.eft}</small>
-                                            <div>{stops ? `${stops} Stop(s)` : 'Direct'}</div>
-                                        </div>
-                                        <div className={styles.airport}>
-                                            <div className={styles.airport__time}>{segment.arrivalTime}</div>
-                                            <div className={styles.airport__code}>{segment.arrivalAirportCode}</div>
-                                        </div>
-                                    </div>
-                                    <div className={styles.segment__tags}>
-                                        {segment.seatsAvailable ? <div className={styles.tag}>
-                                            {segment.seatsAvailable} seats left
-                                        </div> : null}
-                                        {segment.flexibleChange ? <div className={styles.tag}>
-                                            Flexible Change
-                                        </div> : null}
-                                        {segment.flexibleCancellation ? <div className={styles.tag}>
-                                            Flexible Cancellation
-                                        </div> : null}
-                                    </div>
-                                </div>
+                                <div className={styles['price-group__price']} dangerouslySetInnerHTML={{ __html: formatPrice(priceGroup) }} />
                             </div>
-                        })}
-                    </div>
-                    <div className={styles['price-group__price']} dangerouslySetInnerHTML={{ __html: formatPrice(priceGroup) }} />
-                </div>
-            }) : null}
-        </div>
+                        </div>
+                    }}
+                </List>
+            )}
+        </AutoSizer>
         <Flight open={selectedPriceGroup != undefined}
             handleClose={() => { setSelectedPriceGroup(undefined) }}
             position="bottom"
@@ -105,13 +112,12 @@ export default function Search() {
             results={results}
             title="Flight Details"
         />
-        <Modal open={selectedModal}  handleClose={() => { setSelectedModal(undefined) }} />
+        <Modal open={selectedModal} handleClose={() => { setSelectedModal(undefined) }} />
     </React.Fragment>
 }
 
 const Flight = withModal(props => {
     const selectedPriceGroup = React.useRef(props.results.priceGroups.filter(priceGroup => priceGroup.id == props.selectedPriceGroup)).current[0];
-    console.log(selectedPriceGroup);
     return <React.Fragment>
         <div>
             {selectedPriceGroup.groupSegments.map(groupSegment => {
@@ -121,7 +127,6 @@ const Flight = withModal(props => {
                     <div class={flight['segment-label']}>{groupSegment.label}</div>
                     <div class={flight.legs}>
                         {segment.legs.map((leg, i) => {
-                            console.log(leg)
                             return <React.Fragment>
                                 <div class={flight.airport}>
                                     <div class={flight.airport__dot} />
@@ -149,7 +154,7 @@ const Flight = withModal(props => {
         </div>
         <div class={flight.price}>
             Great Price!
-            <strong dangerouslySetInnerHTML={{ __html: formatPrice(selectedPriceGroup) }}/>
+            <strong dangerouslySetInnerHTML={{ __html: formatPrice(selectedPriceGroup) }} />
         </div>
         <div class={flight.footer}>
             <button class="button">Book Flight</button>
@@ -163,4 +168,10 @@ function formatPrice(priceGroup) {
 
 function dateToString(date) {
     return `${date.getDate()} ${months[date.getMonth()]} ${date.getHours()}:${date.getMinutes()}`
+}
+
+function getItemSize(index) { 
+    const element = document.querySelector(`[data-index="${index}"]`);
+    console.log(`[data-index="${index}"]`);
+    return element ? element.offsetHeight: 145 
 }
